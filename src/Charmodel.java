2dd4e2d9dd3d8c287033251e8233aaa079d31dd0
SJDavies2020 <200411361@student.georgianc.on.ca>
2020-10-25 13:54:01 -0400

    Create DB Ulti for record insertion.

--- src/Charmodel.java
+++ src/Charmodel.java
@@ -6,44 +6,142 @@ import org.apache.commons.lang3.*;
 
 public class Charmodel {
 
-    private String charClass;
     private String charRace;
-    private int total;
+    private int totalFighter;
+    private int totalRogue;
+    private int totalWizard;
+    private int totalBarbarian;
+    private int totalCleric;
+    private int totalRanger;
+    private int totalPaladin;
+    private int totalWarlock;
+    private int totalMonk;
+    private int totalBard;
+    private int totalSorcerer;
+    private int totalDruid;
 
-public void characterModel(String charCls, String charRac, int tot)
+
+public void characterModel(String chrRac, int totFighter, int totRogue,int totWizard,int totBarbarian,int totCleric,int totRanger,int totPaladin,int totWarlock,int totMonk,int totBard,int totSorcerer,int totDruid)
 {
-    setCharClass(charCls);
-    setCharRace(charRac);
-    setTotal(tot);
-}
-    public String getCharClass() {
-        return charClass;
-    }
+    setCharRace(chrRac);
+    setTotalFighter(totFighter);
+    setTotalRogue(totRogue);
+    setTotalWizard(totWizard);
+    setTotalBarbarian(totBarbarian);
+    setTotalCleric(totCleric);
+    setTotalRanger(totRanger);
+    setTotalPaladin(totPaladin);
+    setTotalWarlock(totWarlock);
+    setTotalMonk(totMonk);
+    setTotalBard(totBard);
+    setTotalSorcerer(totSorcerer);
+    setTotalDruid(totDruid);
 
-    public void setCharClass(String charCls) {
-        if (StringUtils.isNumeric(charCls)) {
-            throw new IllegalArgumentException("Input must only be text!");
-        } else {
-            this.charClass = charCls;
-        }
-       this.charClass = charClass;
-    }
+}
+
     public String getCharRace() {return charRace;
     }
     public void setCharRace(String charRac) {
         if (StringUtils.isNumeric(charRac)) {
             throw new IllegalArgumentException("Input must only be text!");
         } else {
-            this.charClass = charRac;
+            this.charRace = charRac;
         }
     }
-    public int getTotal() {return total;
-    }
-    public void setTotal(int total) {
-        if (total>0)
-            this.total = total;
-        else
-            throw new IllegalArgumentException("student number must be greater than 0");
+
+    public int getTotalFighter() {
+        return totalFighter;
     }
 
+    public void setTotalFighter(int totalFighter) {
+        this.totalFighter = totalFighter;
+    }
+
+    public int getTotalRogue() {
+        return totalRogue;
+    }
+
+    public void setTotalRogue(int totalRogue) {
+        this.totalRogue = totalRogue;
+    }
+
+    public int getTotalWizard() {
+        return totalWizard;
+    }
+
+    public void setTotalWizard(int totalWizard) {
+        this.totalWizard = totalWizard;
+    }
+
+    public int getTotalBarbarian() {
+        return totalBarbarian;
+    }
+
+    public void setTotalBarbarian(int totalBarbarian) {
+        this.totalBarbarian = totalBarbarian;
+    }
+
+    public int getTotalCleric() {
+        return totalCleric;
+    }
+
+    public void setTotalCleric(int totalCleric) {
+        this.totalCleric = totalCleric;
+    }
+
+    public int getTotalRanger() {
+        return totalRanger;
+    }
+
+    public void setTotalRanger(int totalRanger) {
+        this.totalRanger = totalRanger;
+    }
+
+    public int getTotalPaladin() {
+        return totalPaladin;
+    }
+
+    public void setTotalPaladin(int totalPaladin) {
+        this.totalPaladin = totalPaladin;
+    }
+
+    public int getTotalWarlock() {
+        return totalWarlock;
+    }
+
+    public void setTotalWarlock(int totalWarlock) {
+        this.totalWarlock = totalWarlock;
+    }
+
+    public int getTotalMonk() {
+        return totalMonk;
+    }
+
+    public void setTotalMonk(int totalMonk) {
+        this.totalMonk = totalMonk;
+    }
+
+    public int getTotalBard() {
+        return totalBard;
+    }
+
+    public void setTotalBard(int totalBard) {
+        this.totalBard = totalBard;
+    }
+
+    public int getTotalSorcerer() {
+        return totalSorcerer;
+    }
+
+    public void setTotalSorcerer(int totalSorcerer) {
+        this.totalSorcerer = totalSorcerer;
+    }
+
+    public int getTotalDruid() {
+        return totalDruid;
+    }
+
+    public void setTotalDruid(int totalDruid) {
+        this.totalDruid = totalDruid;
+    }
 }
---
+++ src/DBUtil.java
@@ -0,0 +1,126 @@
+import java.sql.*;
+
+public class DBUtil {
+// User Name and Password
+private static String user="stevendavies";
+private static String password="Bonnied029$";
+
+
+    /**
+     * This method will write a new line to the DataBase and return the CharId
+     * @param newCharacter
+     * @return charId (Primary ID from the DB Table)
+     */
+
+
+public static int insertNewCharacterRace(Charmodel newCharacterRace) throws SQLException {
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    int raceId = -1;
+    ResultSet rs = null;
+
+    try{
+
+        // 1 Define the Connection String
+
+        conn= DriverManager.getConnection("jdbc:mysql://107.180.0.227:3306/Comp1011_Assign1", user,password) ;
+
+        // 2 Create the SQL Command Info
+
+        String sql =  "INSERT INTO `tblCharsStat`(`race`, `class`, `number`) \n" +
+                "VALUES (?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?),(?,?,?)";
+
+        // 3 Create the Prepared Statement (SQl Injection Protection)
+
+        ps = conn.prepareStatement(sql, new String [] {"raceID"});
+
+
+        // 4 Bind the Values
+
+        ps.setString(1, newCharacterRace.getCharRace());
+        ps.setString(2, "FIGHTER");
+        ps.setInt(3, newCharacterRace.getTotalFighter());
+
+        ps.setString(4, newCharacterRace.getCharRace());
+        ps.setString(5, "ROGUE");
+        ps.setInt(6, newCharacterRace.getTotalRogue());
+
+        ps.setString(7, newCharacterRace.getCharRace());
+        ps.setString(8, "WIZARD");
+        ps.setInt(9, newCharacterRace.getTotalWizard());
+
+        ps.setString(10, newCharacterRace.getCharRace());
+        ps.setString(11, "BARBARIAN");
+        ps.setInt(12, newCharacterRace.getTotalBarbarian());
+
+        ps.setString(13, newCharacterRace.getCharRace());
+        ps.setString(14, "CLERIC");
+        ps.setInt(15, newCharacterRace.getTotalCleric());
+
+        ps.setString(16, newCharacterRace.getCharRace());
+        ps.setString(17, "RANGER");
+        ps.setInt(18, newCharacterRace.getTotalRanger());
+
+        ps.setString(19, newCharacterRace.getCharRace());
+        ps.setString(20, "PALADIN");
+        ps.setInt(21, newCharacterRace.getTotalPaladin());
+
+        ps.setString(22, newCharacterRace.getCharRace());
+        ps.setString(23, "WARLOCK");
+        ps.setInt(24, newCharacterRace.getTotalWarlock());
+
+        ps.setString(25, newCharacterRace.getCharRace());
+        ps.setString(26, "MONK");
+        ps.setInt(27, newCharacterRace.getTotalMonk());
+
+        ps.setString(28, newCharacterRace.getCharRace());
+        ps.setString(29, "BARD");
+        ps.setInt(30, newCharacterRace.getTotalBard());
+
+        ps.setString(31, newCharacterRace.getCharRace());
+        ps.setString(32, "SORCERER");
+        ps.setInt(33, newCharacterRace.getTotalSorcerer());
+
+        ps.setString(34, newCharacterRace.getCharRace());
+        ps.setString(35, "DRUID");
+        ps.setInt(36, newCharacterRace.getTotalDruid());
+
+        // 5 execute the SQL and write to the DB
+
+        ps.executeUpdate();
+
+        // 6 Get the returned Character RaceId
+
+        rs = ps.getGeneratedKeys();
+
+        while(rs.next())
+        {
+           raceId = rs.getInt(1);
+        }
+
+    }
+    catch (SQLException e)
+    {
+        e.printStackTrace();
+    } finally {
+        // Clear All Objects/Connections
+        if (conn != null)
+        {
+            conn.close();
+        }
+        if (ps != null)
+        {
+            ps.close();
+        }
+        if (rs !=null)
+        {
+            rs.close();
+        }
+        return raceId;
+    }
+
+}
+
+
+}
