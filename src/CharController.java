780934667418b6fd71d3131d022d18ad09afb3e3
SJDavies2020 <200411361@student.georgianc.on.ca>
2020-10-25 16:57:31 -0400

    Character Controller Defined and Scenebuilder Scene in process.

---
+++ src/CharController.java
@@ -0,0 +1,50 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CharController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField raceID;
+
+    @FXML
+    private TextField totalFighters;
+
+    @FXML
+    private TextField totalRogues;
+
+    @FXML
+    private TextField totalWizards;
+
+    @FXML
+    private TextField totalClerics;
+
+    @FXML
+    private TextField totalPaladins;
+
+    @FXML
+    private TextField totalWarlocks;
+
+    @FXML
+    private TextField totalMonks;
+
+    @FXML
+    private TextField totalBards;
+
+    @FXML
+    private TextField totalBarbarians;
+
+    @FXML
+    private TextField totalRangers;
+
+    @FXML
+    private TextField totalDruids;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Charview.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("newCharview.fxml"));
+        primaryStage.setScene(new Scene(root,  600, 400));
+        primaryStage.setTitle("Character Race Builder");
+        primaryStage.show();
     }
 }
